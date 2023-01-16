194227c42296311d48fcedd30fdb0e7987849107
NathanCioppa <112644713+NathanCioppa@users.noreply.github.com>
2023-01-16 12:15:56 -0500

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,7 +1,10 @@
 function getToken() {
     const hash = window.location.hash
     const token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1]
-    document.getElementById('showToken').value = token
+    const tokenInput = document.getElementById('showToken')
+    tokenInput.value = token
 
+    tokenInput.select()
+    tokenInput.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(tokenInput.value)
 }
-getToken()
