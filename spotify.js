ebd378466707f8cb922343211dee570e7ca32233
NathanCioppa <112644713+NathanCioppa@users.noreply.github.com>
2023-01-29 14:49:44 -0500

    should work with mobile app now

--- spotify.js
+++ spotify.js
@@ -1,10 +1,20 @@
 function getToken() {
+
+    const mobileCode = mobileAuthCode()
+
     const hash = window.location.hash
-    const token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1]
+    const token = mobileCode === null ? hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1] : mobileCode
     const tokenInput = document.getElementById('showToken')
     tokenInput.value = token
 
     tokenInput.select()
     tokenInput.setSelectionRange(0, 99999)
     navigator.clipboard.writeText(tokenInput.value)
+}
+
+function mobileAuthCode() {
+    const url = window.location.href
+    const params = new URLSearchParams(url)
+    const code = params.get('code')
+    return code
 }
